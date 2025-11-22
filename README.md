f83658132d162899c596816ea20da0ecbf48ed80
rodrigo-jdelval <rodrigo-jdelval@users.noreply.github.com>
2025-11-22 11:06:49 +0000

    deploy: 85e61f7bc579534a0d646a0d9c557536831f93be

---
+++ README.md
@@ -0,0 +1,1 @@
+# naaf-training-engine
---
+++ training_engine_bg.wasm.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const init_engine: () => [number, number];
+export const train_step_simulated: (a: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
